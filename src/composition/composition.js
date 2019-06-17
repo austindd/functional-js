a5109efa1d18761dff48e2ee71071d0aeb5dadef
austinddavis <austindanieldavis@gmail.com>
2019-06-17 16:01:12 -0500

    added more tests for the 'curry' function

--- src/composition/composition.js
+++ src/composition/composition.js
@@ -1,42 +1,35 @@
 
 // Allows passing "tuples" (implemented as arrays) as
-// individual function arguments
+// individual function arguments.
+// If one parameter takes an array, the array should be
+// nested within the "tuple" (arguments) array.
+
 function enableTuples(fn) {
   return function allowingTuples(args) {
     if (Array.isArray(args)) return fn.apply(fn, args)
-    else return fn(args)
+    else return fn(args);
     
   };
-}
-function curry(f) {
-return function curried(...args) {
-let ctx = this;
-if (args.length >= f.length) {
-return f.call(ctx, ...args);
-}
-return function(...args2) {
-return curried.call(ctx, ...args, ...args2);
-}
-}
 }
 
 // Warning: Do not use 'curry' with default parameters!!
 // Function.prototype.length will not count any parameters
 // that come after a default parameter!
 
-function _curry(fn) {
+function curry(f) {
   return function curried(...args) {
-    if (args.length >= fn.length) { // (1)
-      return fn.apply(this, args);
-    } else {
-      return function pass(...args2) { // (2)
-        return curried.apply(this, args.concat(args2));
-      }
+    let ctx = this; // caching the context
+    if (args.length >= f.length) {
+      return f.apply(ctx, args);
     }
-  };
+    return function(...args2) {
+    return curried.apply(ctx, args.concat(args2));
+    }
+  }
 }
 
-function pipe (...fns) {
+
+function pipe(...fns) {
   return (...args) => {
     let x = fns[0](...args);
     for (let i = 1, n = fns.length; i < n; i++) {
@@ -52,7 +45,7 @@ function trace (x) {
 };
 
 module.exports = {
-  enableTuples,
+  enableTuples: enableTuples,
   curry: curry,
   pipe: pipe,
   trace: trace,
--- src/composition/composition.test.js
+++ src/composition/composition.test.js
@@ -12,21 +12,26 @@ function testComposition() {
       testArr.push(func1 === func1);
 
       const func2 = func1(3);
-      testArr.push(typeof func2 === "function");
+      // Is 'curry' returning a funtion?
+      testArr.push(typeof func2 === "function"); 
+      // Is 'curry' waiting for all arguments to be provided?
       testArr.push(typeof func2()()()()() === "function");
       
       const func3 = func2(4);
-      testArr.push(typeof func3 === "function")
+      testArr.push(typeof func3 === "function");
       testArr.push(typeof func3()()()()() === "function");
 
       const final = func3(8);
-      testArr.push(typeof final === "number") 
-      testArr.push(final === 35)
+      // Is the function completed when all arguments are provided?
+      testArr.push(typeof final === "number");
+      testArr.push(final === 35);
 
-      return mergeResults(testArr);
+      return allTrue(testArr);
     },
 
-    function curry2() {
+    // Testing whether the 'curry' function works for prototype
+    // methods on classes.
+    function curry2() { 
       class Klass {
         constructor(val) {
           this.val = val;
@@ -35,15 +40,12 @@ function testComposition() {
           return a + b + c;
         }
         valueOf() {
-        
+          return this.value;
         }
       }
       Klass.prototype.add = C.curry(Klass.prototype.add);
-      
-
       const klass = new Klass(2);
       const add0 = klass.add(0);
-
       const test = [
         typeof klass === "object",
         typeof add0 === "function",
@@ -51,8 +53,65 @@ function testComposition() {
         add0(2)(8) === 10,
       ];
 
-      console.dir(test);
-      return mergeResults(test);
+      return allTrue(test);
+    },
+
+    // Does context work properly within class methods?
+    function curry3() {
+      class Klass {
+        constructor(val) {
+          this.val = val;
+        }
+        getThis(a, b) {
+          return this;
+        }
+        valueOf() {
+          return this.value;
+        }
+      }
+      
+      Klass.prototype.getThis = C.curry(Klass.prototype.getThis);
+      const klass = new Klass(0);
+      const getKlass = klass.getThis(1);
+
+      const test = [
+        typeof klass === "object",
+        typeof getKlass === "function",
+        klass.getThis(4)(5) === klass 
+      ];
+
+      return allTrue(test);
+    },
+    
+    // Can curried functions still be fully applied with tuples?
+    function curry4() {
+      const _add = (a, b) => a + b;
+      const _concat3 = (str1, str2, str3) => "" + str1 + str2 + str3;
+
+      const add = C.curry(_add);
+      const concat3 = C.curry(_concat3);
+
+      const test = [
+        typeof add(1) === "function",
+        typeof add(1)(2) === "number",
+        typeof add(1, 2) === "number",
+        add(1)(2) === 3,
+        add(1, 2) === 3,
+        add()(1, 2) === 3,
+        typeof concat3("1") === "function",
+        typeof concat3("1")("2")("3") === "string",
+        typeof concat3("1", "2", "3") === "string",
+        concat3(1)(2)(3) === "123",
+        concat3(1, 2, 3) === "123",
+        concat3(1, 2)(3) === "123",
+        concat3(1)(2, 3) === "123",
+        concat3()(1, 2, 3) === "123" 
+      ];
+
+      console.dir(add(1,2));
+      console.dir(concat3(1, 2, 3));
+      console.dir(test.map((item, i) => [i, item]));
+      return allTrue(test);
     }
 
   ];
@@ -60,7 +119,7 @@ function testComposition() {
   return tests;
 }
 
-function mergeResults(arr) {
+function allTrue(arr) {
   return arr.reduce((didPass, isTrue) => { 
     if (didPass !== true) {
       return false;
@@ -75,7 +134,7 @@ function inject(dep) {
       return (function() {
         _dep.call(this)
         context.call(this)
-      })()
+      })();
     }
   }
 }
